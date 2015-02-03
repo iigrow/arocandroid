6614ca7f33993d0135a4fd4d126d1eaf85a5bfb2
xuejpGitHub <xuejpmail@gmail.com>
2015-02-03 23:08:30 +0800

    add some function

--- Study/src/com/aroc/study/SendMessage.java
+++ Study/src/com/aroc/study/SendMessage.java
@@ -1,11 +1,18 @@
 package com.aroc.study;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
+import android.R.string;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Path;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.view.View;
@@ -57,11 +64,11 @@ public class SendMessage extends Activity implements OnClickListener {
 					null, null);
 		}
 	}
-	
+
 	/**
 	 * 拨打电话
 	 */
-	public void callNumber(){
+	public void callNumber() {
 		EditText txtTelNumber = (EditText) this.findViewById(R.id.etTelnumber);
 		String telNumber = txtTelNumber.getText().toString();
 		if (TextUtils.isEmpty(telNumber)) {
@@ -72,6 +79,45 @@ public class SendMessage extends Activity implements OnClickListener {
 		Intent intent = new Intent();
 		intent.setAction(Intent.ACTION_CALL);
 		intent.setData(Uri.parse("tel:" + telNumber));
-		startActivity(intent);		
+		startActivity(intent);
 	}
+
+	/**
+	 * 保存文件
+	 */
+	public void saveCache(String text) throws Exception {
+		// google在data/data目录下专门按照包名建立了一个缓存目录，用于存储文件，不允许在data/app存储文件
+		try {
+			// Context context=(Context)this;
+			// this.getFilesDir() //data/data/files
+			// this.openFileInput(name);
+			// this.openFileOutput(name, mode);
+			File file = new File(this.getFilesDir(), "tmp.dat");
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(text.getBytes());
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public void saveSDCard(String text) throws Exception {
+		try {
+			// sd卡在mnt/sdcard目录下 也可以直接存sdcard（是对mnt/sdcard的引用）
+			// 需要写权限信息（开发者选项中有选项可以设置读sd卡要权限）
+			String filepath = Environment.getExternalStorageDirectory().getPath();
+			if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+				// 有SD卡
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public long getSDCardSize() {
+		StatFs statFs = new StatFs(Environment.getExternalStorageDirectory()
+				.getPath());
+		
+		return statFs.getBlockCountLong() * statFs.getBlockSizeLong();
+	}
+
 }
